7ee60491c06ca450db6ea596b99ed62072a7266c
JovannyRch <43225215+JovannyRch@users.noreply.github.com>
2020-07-11 09:57:11 -0500

    Loging and logout

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useReducer, useEffect } from 'react'
 import { AppRouter } from './routers/AppRouter'
+import { authReducer } from './auth/authReducer';
+import { AuthContext } from './auth/AuthContext';
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('user')) || {
+        logged: false,
+    };
+}
 
 export const HeroesApp = () => {
+
+
+    const [user, dispatchUser] = useReducer(authReducer, {}, init);
+
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(user))
+    }, [user])
+
     return (
-        <AppRouter />
-    )
+        <AuthContext.Provider value={{ user, dispatchUser }}>
+            <AppRouter />
+        </AuthContext.Provider>
+    );
 }
--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
     const handleLogin = () => {
+        const action = {
+            type: types.login,
+            payload: {
+                id: 1,
+                name: 'Jovanny Rch',
+            }
+        };
+        dispatchUser(action);
         history.replace('/');
     };
+    const { user, dispatchUser } = useContext(AuthContext);
+
+
     return (
         <div>
             <h1>Login Screen</h1>
+            {JSON.stringify(user.logged)}
             <hr />
             <div className="text-left">
                 <button onClick={handleLogin} className="btn btn-success">Login</button>
